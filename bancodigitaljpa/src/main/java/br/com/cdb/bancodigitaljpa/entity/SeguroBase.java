931f3e2d1196e04ed4f1ca2d8a9d25d60bf1b3d2
Bruna <brunamoreira@artedeviver.org.br>
2025-04-06 09:36:54 -0300

    Ajustes em seguros.

--- bancodigitaljpa/src/main/java/br/com/cdb/bancodigitaljpa/entity/SeguroBase.java
+++ bancodigitaljpa/src/main/java/br/com/cdb/bancodigitaljpa/entity/SeguroBase.java
@@ -1,104 +1,104 @@
-package br.com.cdb.bancodigitaljpa.entity;
-
-import java.beans.Transient;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import br.com.cdb.bancodigitaljpa.enums.TipoSeguro;
-import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
-import jakarta.persistence.DiscriminatorType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Size;
-
-@Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "tipo_seguro", discriminatorType = DiscriminatorType.STRING)
-public abstract class SeguroBase {
-	
-	//A
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id_seguro;
-	
-	@Column(unique = true, nullable = false, updatable = false)
-	private String numApolice;
-	
-	@ManyToOne
-	@JoinColumn(name = "id_cartao", nullable = false, updatable = false)
-	private CartaoCredito cartaoCredito;
-	
-	@Column(name = "data_contratacao", nullable = false, updatable = false)
-	@JsonFormat(pattern = "dd-MM-yyyy")
-	private LocalDate dataContratacao;
-	
-	@Column(nullable = false, precision = 19, scale = 2)
-	private BigDecimal valorApolice;
-	
-	@Size(max=300, message="A descrição deve ter no máximo 300 caracteres.")
-	private String descricaoCondicoes;
-	
-	@Column(nullable = false, precision = 19, scale = 2)
-	private BigDecimal premioApolice;
-
-	//G&S
-	public Long getId_seguro() {
-		return id_seguro;
-	}
-	public String getNumApolice() {
-		return numApolice;
-	}
-	public CartaoCredito getCartaoCredito() {
-		return cartaoCredito;
-	}
-	public LocalDate getDataContratacao() {
-		return dataContratacao;
-	}
-	public BigDecimal getValorApolice() {
-		return valorApolice;
-	}
-	public void setValorApolice(BigDecimal valorApolice) {
-		this.valorApolice = valorApolice;
-	}
-	public String getDescricaoCondicoes() {
-		return descricaoCondicoes;
-	}
-	public void setDescricaoCondicoes(String descricaoCondicoes) {
-		this.descricaoCondicoes = descricaoCondicoes;
-	}
-	public BigDecimal getPremioApolice() {
-		return premioApolice;
-	}
-	public void setPremioApolice(BigDecimal premioApolice) {
-		this.premioApolice = premioApolice;
-	}
-
-	//C
-	public SeguroBase(CartaoCredito ccr) {
-		super();
-		this.cartaoCredito = ccr;
-		this.dataContratacao = LocalDate.now();
-		this.numApolice = gerarNumeroApolice();
-	}
-	public SeguroBase() {}
-
-	//tipoSeguro
-	//M
-	private String gerarNumeroApolice() {
-		return UUID.randomUUID().toString().substring(0, 8).toUpperCase();
-	}
-	
-	@Transient
-	public abstract TipoSeguro getTipoSeguro();
-	
-}
+//package br.com.cdb.bancodigitaljpa.entity;
+//
+//import java.beans.Transient;
+//import java.math.BigDecimal;
+//import java.time.LocalDate;
+//import java.util.UUID;
+//
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//
+//import br.com.cdb.bancodigitaljpa.enums.TipoSeguro;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.DiscriminatorColumn;
+//import jakarta.persistence.DiscriminatorType;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Inheritance;
+//import jakarta.persistence.InheritanceType;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.validation.constraints.Size;
+//
+//@Entity
+//@Inheritance(strategy = InheritanceType.JOINED)
+//@DiscriminatorColumn(name = "tipo_seguro", discriminatorType = DiscriminatorType.STRING)
+//public abstract class SeguroBase {
+//	
+//	//A
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id_seguro;
+//	
+//	@Column(unique = true, nullable = false, updatable = false)
+//	private String numApolice;
+//	
+//	@ManyToOne
+//	@JoinColumn(name = "id_cartao", nullable = false, updatable = false)
+//	private CartaoCredito cartaoCredito;
+//	
+//	@Column(name = "data_contratacao", nullable = false, updatable = false)
+//	@JsonFormat(pattern = "dd-MM-yyyy")
+//	private LocalDate dataContratacao;
+//	
+//	@Column(nullable = false, precision = 19, scale = 2)
+//	private BigDecimal valorApolice;
+//	
+//	@Size(max=300, message="A descrição deve ter no máximo 300 caracteres.")
+//	private String descricaoCondicoes;
+//	
+//	@Column(nullable = false, precision = 19, scale = 2)
+//	private BigDecimal premioApolice;
+//
+//	//G&S
+//	public Long getId_seguro() {
+//		return id_seguro;
+//	}
+//	public String getNumApolice() {
+//		return numApolice;
+//	}
+//	public CartaoCredito getCartaoCredito() {
+//		return cartaoCredito;
+//	}
+//	public LocalDate getDataContratacao() {
+//		return dataContratacao;
+//	}
+//	public BigDecimal getValorApolice() {
+//		return valorApolice;
+//	}
+//	public void setValorApolice(BigDecimal valorApolice) {
+//		this.valorApolice = valorApolice;
+//	}
+//	public String getDescricaoCondicoes() {
+//		return descricaoCondicoes;
+//	}
+//	public void setDescricaoCondicoes(String descricaoCondicoes) {
+//		this.descricaoCondicoes = descricaoCondicoes;
+//	}
+//	public BigDecimal getPremioApolice() {
+//		return premioApolice;
+//	}
+//	public void setPremioApolice(BigDecimal premioApolice) {
+//		this.premioApolice = premioApolice;
+//	}
+//
+//	//C
+//	public SeguroBase(CartaoCredito ccr) {
+//		super();
+//		this.cartaoCredito = ccr;
+//		this.dataContratacao = LocalDate.now();
+//		this.numApolice = gerarNumeroApolice();
+//	}
+//	public SeguroBase() {}
+//
+//	//tipoSeguro
+//	//M
+//	private String gerarNumeroApolice() {
+//		return UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+//	}
+//	
+//	@Transient
+//	public abstract TipoSeguro getTipoSeguro();
+//	
+//}
--- bancodigitaljpa/src/main/java/br/com/cdb/bancodigitaljpa/entity/SeguroFraude.java
+++ bancodigitaljpa/src/main/java/br/com/cdb/bancodigitaljpa/entity/SeguroFraude.java
@@ -1,48 +1,48 @@
-package br.com.cdb.bancodigitaljpa.entity;
-
-import java.beans.Transient;
-import java.math.BigDecimal;
-
-import br.com.cdb.bancodigitaljpa.enums.Status;
-import br.com.cdb.bancodigitaljpa.enums.TipoSeguro;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-
-@Entity
-@DiscriminatorValue("FRAUDE")
-public class SeguroFraude extends SeguroBase {
-	
-	@Enumerated(EnumType.STRING)
-	private Status statusSeguroFraude;
-
-	//G&S
-	public Status getStatusSeguroFraude() {
-		return statusSeguroFraude;
-	}
-	public void setStatusSeguroFraude(Status statusSeguroFraude) {
-		this.statusSeguroFraude = statusSeguroFraude;
-	}
-	
-	//C
-	public SeguroFraude (CartaoCredito ccr) {
-		super(ccr);
-		this.statusSeguroFraude = Status.ATIVADO;
-		this.setDescricaoCondicoes(TipoSeguro.FRAUDE.getDescricao());
-		this.setValorApolice(BigDecimal.valueOf(5000.00));
-		this.setPremioApolice(BigDecimal.ZERO);
-	}
-	
-	//M
-	@Override
-	@Transient
-	public TipoSeguro getTipoSeguro() {
-		return TipoSeguro.FRAUDE;
-	}
-	public String getTipo() {
-		return TipoSeguro.FRAUDE.getDescricao();
-	}
-	
-
-}
+//package br.com.cdb.bancodigitaljpa.entity;
+//
+//import java.beans.Transient;
+//import java.math.BigDecimal;
+//
+//import br.com.cdb.bancodigitaljpa.enums.Status;
+//import br.com.cdb.bancodigitaljpa.enums.TipoSeguro;
+//import jakarta.persistence.DiscriminatorValue;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.EnumType;
+//import jakarta.persistence.Enumerated;
+//
+//@Entity
+//@DiscriminatorValue("FRAUDE")
+//public class SeguroFraude extends SeguroBase {
+//	
+//	@Enumerated(EnumType.STRING)
+//	private Status statusSeguroFraude;
+//
+//	//G&S
+//	public Status getStatusSeguroFraude() {
+//		return statusSeguroFraude;
+//	}
+//	public void setStatusSeguroFraude(Status statusSeguroFraude) {
+//		this.statusSeguroFraude = statusSeguroFraude;
+//	}
+//	
+//	//C
+//	public SeguroFraude (CartaoCredito ccr) {
+//		super(ccr);
+//		this.statusSeguroFraude = Status.ATIVADO;
+//		this.setDescricaoCondicoes(TipoSeguro.FRAUDE.getDescricao());
+//		this.setValorApolice(BigDecimal.valueOf(5000.00));
+//		this.setPremioApolice(BigDecimal.ZERO);
+//	}
+//	
+//	//M
+//	@Override
+//	@Transient
+//	public TipoSeguro getTipoSeguro() {
+//		return TipoSeguro.FRAUDE;
+//	}
+//	public String getTipo() {
+//		return TipoSeguro.FRAUDE.getDescricao();
+//	}
+//	
+//
+//}
