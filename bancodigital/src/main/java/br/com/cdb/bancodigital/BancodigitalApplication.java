39bdb78a8aea3fa2cb93be96ee659fb6e1143e98
Bruna <brunamoreira@artedeviver.org.br>
2025-05-07 13:57:01 -0300

    Ajuste de notations nas classes Role e Application

--- bancodigital/src/main/java/br/com/cdb/bancodigital/BancodigitalApplication.java
+++ bancodigital/src/main/java/br/com/cdb/bancodigital/BancodigitalApplication.java
@@ -3,8 +3,10 @@ package br.com.cdb.bancodigital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class BancodigitalApplication {
 
--- bancodigital/src/main/java/br/com/cdb/bancodigital/model/enums/Role.java
+++ bancodigital/src/main/java/br/com/cdb/bancodigital/model/enums/Role.java
@@ -3,7 +3,6 @@ package br.com.cdb.bancodigital.model.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
 
