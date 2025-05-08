d71a408108e7cbc8d89b04955e6b859ed6d009bf
Bruna <brunamoreira@artedeviver.org.br>
2025-05-08 18:42:01 -0300

    Classes sem uso removidas

--- bancodigital/src/main/java/br/com/cdb/bancodigital/exceptions/ErrorMessages.java
+++
@@ -1,7 +0,0 @@
-package br.com.cdb.bancodigital.exceptions;
-
-public final class ErrorMessages {
-	public static final String CLIENTE_NAO_ENCONTRADO = "Cliente ID %d não encontrado.";
-	
-
-}
--- bancodigital/src/test/java/br/com/cdb/bancodigital/service/impl/ClienteServiceImplTest.java
+++
@@ -1,5 +0,0 @@
-package br.com.cdb.bancodigital.service.impl;
-
-public class ClienteServiceImplTest {
-
-}
