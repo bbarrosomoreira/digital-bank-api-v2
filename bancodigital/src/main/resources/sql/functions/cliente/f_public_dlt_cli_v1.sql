8875d4eb717c303312b904bf791de3238594cae2
Bruna <brunamoreira@artedeviver.org.br>
2025-05-29 12:14:38 -0300

    Ajuste de function para excluir seu relacionamento com a tabela de endereço antes de excluir o cliente.

--- bancodigital/src/main/resources/sql/functions/cliente/f_public_dlt_cli_v1.sql
+++ bancodigital/src/main/resources/sql/functions/cliente/f_public_dlt_cli_v1.sql
@@ -1,17 +1,27 @@
-CREATE OR REPLACE FUNCTION public_dlt_cli_v1(
-	p_id BIGINT
-)
-RETURNS INT
+-- FUNCTION: public.dlt_cli_v1(bigint)
 
-LANGUAGE 'plpgsql'
+-- DROP FUNCTION IF EXISTS public.dlt_cli_v1(bigint);
 
+CREATE OR REPLACE FUNCTION public.dlt_cli_v1(
+	p_id bigint)
+    RETURNS integer
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
 AS $BODY$
-
 BEGIN
-		DELETE FROM cliente
-		WHERE id = p_id;
+	-- Exclui os vínculos com endereços
+	DELETE FROM endereco_cliente
+	WHERE cliente_id = p_id;
 
-		RETURN FOUND::INT;
+	-- Depois exclui o cliente
+	DELETE FROM cliente
+	WHERE id = p_id;
 
+	-- Retorna 1 se o cliente foi excluído, 0 caso contrário
+	RETURN FOUND::INT;
 END;
-$BODY$
+$BODY$;
+
+ALTER FUNCTION public.dlt_cli_v1(bigint)
+    OWNER TO postgres;
